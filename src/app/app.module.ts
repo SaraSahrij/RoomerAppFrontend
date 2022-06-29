402d3b1382d7993b31697d13f7cf8e92334afc51
Hannan-Ali <abdulhannan.ali002@stud.fh-dortmund.de>
2022-06-29 14:35:21 +0200

    Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import{MatInputModule} from "@angular/material/input";
 import{MatButtonModule} from "@angular/material/button";
 import{MatCardModule} from '@angular/material/card';
 import { CoronaStatusComponent } from './corona-status/corona-status.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    CoronaStatusComponent
+    CoronaStatusComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
